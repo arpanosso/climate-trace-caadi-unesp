d35f879118bd973934995c7d6cff54de8d5b013e
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2024-03-18 20:56:58 -0300

    faxina rds

--- data-raw/faxina.R
+++ data-raw/faxina.R
@@ -1,5 +1,15 @@
 library(tidyverse)
-dados <- readr::read_rds("data/emissions_sources.rds")
+dados <- readr::read_rds("data-raw/emissions_sources.rds")
 dados <- dados %>%
  filter(sigla_uf == "SP")
-writexl::write_xlsx(dados,"data/sp-emissions-sources.xlsx")
+
+sub_sector <- dados$sub_sector |> unique()
+
+for(i in seq_along(sub_sector)){
+  string_ss <- sub_sector[i]
+  df <- dados |>
+    filter(sub_sector == string_ss)
+  readr::write_rds(df,
+                  paste0("data/",
+                         sub_sector[i],".rds"))
+}
